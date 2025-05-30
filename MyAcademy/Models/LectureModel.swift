56a3d4479d220d51bc3f9b94527ee52497d402cc
s0-sh1r0 <sou.create04@gmail.com>
2025-05-31 01:59:41 +0900

    Model修正

--- MyAcademy/Models/LectureModel.swift
+++ MyAcademy/Models/LectureModel.swift
@@ -3,6 +3,7 @@ import Foundation
 struct Lecture {
     let id: String = UUID().uuidString
     let name: String
+    let user: User
     let dayOfWeek: String
     let period: String
     let teacher: String
