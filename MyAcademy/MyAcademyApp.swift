f99e41c14f18aae955efe443bbf44a4c5b054e18
s0-sh1r0 <sou.create04@gmail.com>
2025-05-30 23:56:36 +0900

    Initial Commit

---
+++ MyAcademy/MyAcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAcademyApp.swift
+//  MyAcademy
+//
+//  Created by 牛田壮志郎 on 2025/05/30.
+//
+
+import SwiftUI
+
+@main
+struct MyAcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
